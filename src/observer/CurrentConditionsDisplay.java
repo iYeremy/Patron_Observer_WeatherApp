82a630de4530d8287100e1ea588da7a9703fa398
iyeremy <yeremyvegap@gmail.com>
2026-02-16 13:05:45 -0500

    feat: implement CurrentConditions display

---
+++ src/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,34 @@
+package observer;
+import subject.Subject;
+import subject.WeatherData;
+
+public class CurrentConditionsDisplay implements Observer, DisplayInterface{
+
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData){
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display(){
+        System.out.println("Current conditions: " +
+                temperature + "C, " +
+                humidity + "% humidity and " + pressure + "");
+    }
+
+    @Override
+    public void update(){
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        this.pressure = weatherData.getPressure();
+
+        display();
+        
+    }
+
+}
